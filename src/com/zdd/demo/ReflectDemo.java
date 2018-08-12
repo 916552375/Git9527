a64ffcd4d330c9156aa42c41be32c669170fd992
916552375 <916552375@qq.com>
2018-08-12 11:21:56 +0800

    add comment

--- src/com/zdd/demo/ReflectDemo.java
+++ src/com/zdd/demo/ReflectDemo.java
@@ -8,7 +8,10 @@ public class ReflectDemo {
 	public String zdd(String prefect, Integer x, Integer y) {
 		return prefect + x + y;
 	}
-
+/**
+ * 
+ * @param number
+ */
 	public void print(int number) {
 		if (number > 0) {
 			for (int i = 0; i < number; i++) {
